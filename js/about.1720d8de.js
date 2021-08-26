6613cdfd2a9f6d4b30f2c3c236d43d37dbf8a043
RoseBud13 <xiongkaijie13@gmail.com>
2021-08-26 17:56:18 +0800

    deploy

---
+++ js/about.1720d8de.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{c1c1:function(c,t,e){},f820:function(c,t,e){"use strict";e.r(t);var n=e("7a23"),u={id:"about"},a=Object(n["e"])("h1",{class:"header"},"Happy Birthday!",-1),d=Object(n["e"])("p",null,"新的一岁，更加开心！",-1),i=[a,d];function o(c,t){return Object(n["q"])(),Object(n["d"])("div",u,i)}e("fd60");const r={};r.render=o;t["default"]=r},fd60:function(c,t,e){"use strict";e("c1c1")}}]);
+//# sourceMappingURL=about.1720d8de.js.map
---
+++ precache-manifest.5110e7a2df4549e8727610e301cf8554.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ed3ab90ca7d22e8221ee8a0ea4d517",
+    "url": "/birthday/audio/happy-birthday.mp3"
+  },
+  {
+    "revision": "10d7abfbd20a1eecbc23",
+    "url": "/birthday/css/about.17cf9d48.css"
+  },
+  {
+    "revision": "d32f844f5f77e1d3998d",
+    "url": "/birthday/css/index.2b223d6c.css"
+  },
+  {
+    "revision": "6800561306331fa0e812286bbe6928db",
+    "url": "/birthday/img/birthday.png"
+  },
+  {
+    "revision": "860a0e68b2be3663a93dbb4a61b1a469",
+    "url": "/birthday/index.html"
+  },
+  {
+    "revision": "10d7abfbd20a1eecbc23",
+    "url": "/birthday/js/about.1720d8de.js"
+  },
+  {
+    "revision": "46a1a6fb9f6abc126e45",
+    "url": "/birthday/js/chunk-vendors.48ebb40b.js"
+  },
+  {
+    "revision": "d32f844f5f77e1d3998d",
+    "url": "/birthday/js/index.23d1ea69.js"
+  },
+  {
+    "revision": "72e8a95752985a2f05b8ce283f3b7738",
+    "url": "/birthday/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/birthday/robots.txt"
+  }
+]);
